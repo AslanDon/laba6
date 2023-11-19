fdc0a2753112206783fae8b1aef214a4d4e8ab57
99499 <99499@DESKTOP-NUA3EGJ>
2023-11-19 23:12:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Vvedite dlinnu stroki:");
+int stroka_size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite dlinnu stolbca:");
+int stolbec_size = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[stroka_size, stolbec_size];
+
+for(int i = 0; i < stroka_size; i++)
+{
+    for(int j = 0; j < stolbec_size; j++)
+    {
+        Console.WriteLine($"Vvedite element po indexu {i},{j}");
+        matrix[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+for (int i = 0; i < stroka_size; i++)
+{
+    for (int j = 0; j < stolbec_size; j++)
+    {
+        if (matrix[i,j] % 2 != 0)
+        {
+            Console.WriteLine(matrix[i,j]);
+        }
+    }
+}
+
